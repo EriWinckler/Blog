05d02437a14987d03026a2a830c68577c0cb9453
Eridan <efwinckler@gmail.com>
2020-08-30 13:29:59 -0400

    first commit

---
+++ src/components/Home.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+function Home() {
+  const posts = [
+      {
+          id: 'id-1',
+          title: 'dummy title',
+          content: 'dummy content'
+      },
+      {
+        id: 'id-2',
+        title: 'dummy title 2',
+        content: 'dummy content 2'
+    }
+  ];
+
+
+  return (
+    <div >
+      <ul>
+          {posts.map(p => (
+              <li>
+                  <h3><a href="">{p.title}</a></h3>
+                  <p>{p.content}</p>
+              </li>
+          ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Home;
